8a333e0f09e8285d03538c74a307766fa60d4038
DillonSir <dillonsirmans1@gmail.com>
2022-09-11 23:01:28 -0400

    cleaned up the nav bar and added a menu to pop up when at smaller dimensions

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
+
 const switchElement = document.querySelector(".switch");
 
 switchElement.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+});
+
+// for the menu button in the nav bar when at the correct resolution//
+
+const toggleButton = document.getElementById("toggle-button");
+const naviList = document.getElementById("navi-list");
+const iSwitch = document.getElementById("iswitch");
+toggleButton.addEventListener("click", () => {
+  naviList.classList.toggle("active");
+  iSwitch.classList.toggle("active");
 });
